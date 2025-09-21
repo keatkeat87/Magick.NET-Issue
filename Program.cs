e7c806555d30a89b5dcc86b803bc53577fbbe237
Derrick Yam <keatkeat87@live.com>
2025-09-21 23:11:20 +0800

    ok

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ImageMagick;
+
+var rootPath = Path.Combine(AppContext.BaseDirectory, @"..\..\..\");
+using var image = new MagickImage(Path.Combine(rootPath, @"logo.png"));
+image.Resize(0, 80);
+
+// logo-edited.avif and logo-edited.png look different
+await image.WriteAsync(Path.Combine(rootPath, @"logo-edited.avif"), MagickFormat.Avif);
+await image.WriteAsync(Path.Combine(rootPath, @"logo-edited.png"), MagickFormat.Png);
+
+image.Quality = 100;
+// logo-edited-100 and logo-edited-100 look fine
+await image.WriteAsync(Path.Combine(rootPath, @"logo-edited-100.avif"), MagickFormat.Avif);
+await image.WriteAsync(Path.Combine(rootPath, @"logo-edited-100.png"), MagickFormat.Png);
